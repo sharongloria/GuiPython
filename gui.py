2ec863cb70207dd40a1302ecd381cf4c7f42991c
sharongloria <sharongloria96@gmail.com>
2024-04-11 12:36:57 +0300

    Initial commit

--- gui.py
+++ gui.py
@@ -22,13 +22,13 @@ gui.geometry("300x200")
 # b = Button(gui, text="Click Here", command=inp)
 # b.place(x=50, y=50)
 
-# frame = Frame(gui)
-# frame.pack()
-# animals = ['Cow', 'Dog', 'Lion', 'Monkey', 'Giraffe']
-# combo = ttk.Combobox(frame, values=animals)
-# combo.set('Select an animal')
-# combo.pack(padx=5, pady=5)
-# gui.mainloop()
+frame = Frame(gui)
+frame.pack()
+animals = ['Cow', 'Dog', 'Lion', 'Monkey', 'Giraffe']
+combo = ttk.Combobox(frame, values=animals)
+combo.set('Select an animal')
+combo.pack(padx=5, pady=5)
+gui.mainloop()
 
 frame = Frame(gui)
 
